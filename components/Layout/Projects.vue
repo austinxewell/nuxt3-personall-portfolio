1ae19c7628dcf8e7778d03ae58d3734d8cf90907
Austin Ewell <austin.ewell86@gmail.com>
2025-12-20 05:35:58 -0700

    updated to use api

--- components/Layout/Projects.vue
+++ components/Layout/Projects.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="p-10">
+    <div v-if="isLoading">Is Loading ...</div>
+    <div v-else class="p-10">
         <header class="w-full lg:w-3/5 mb-10">
             <h2 class="text-5xl font-bold pb-1 border-b-2 w-fit">Portfolio</h2>
             <p class="text-2xl font-bold mt-6">These builds are
-                <strong class="text-yellow-600">polished</strong> with purpose —
+                <strong class="text-yellow-600">polished</strong> with purpose,
                 <strong class="text-yellow-600">refined</strong> to bring both form and function to the forefront.
             </p>
         </header>
@@ -18,5 +19,16 @@
 </template>
 
 <script setup lang='ts'>
-import { projects } from '~/data/projects'
+import { useProjectsStore } from '#imports'
+
+const projectsStore = useProjectsStore()
+
+const { projects } = storeToRefs(projectsStore)
+const isLoading = ref(false)
+
+onMounted(async() => {
+    isLoading.value = true
+    await projectsStore.fetchProjects()
+    isLoading.value = false
+})
 </script>
