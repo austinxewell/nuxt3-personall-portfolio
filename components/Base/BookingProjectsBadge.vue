46beaa9857b5a029b056ade60b3185d46eb691c5
Austin Ewell <austin.ewell86@gmail.com>
2026-09-11 12:57:20 -0600

    refactor: adjusted hero section

--- components/Base/BookingProjectsBadge.vue
+++ components/Base/BookingProjectsBadge.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="absolute -top-12 sm:top-0 right-0 sm:right-6 z-30">
+        <div class="hidden sm:block absolute inset-0 -z-10 bg-yellow-400/40 dark:bg-yellow-500/30 blur-3xl rounded-full" />
         <img
             :src="badgeSrc"
             alt="Booking Projects Badge"
