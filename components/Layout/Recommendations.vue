3a1bdead4fed99d0b0025046b99148ce25c7d044
Austin Ewell <austin.ewell86@gmail.com>
2026-09-21 11:16:32 -0600

    :bug: updated hydration timing issue and undefined handling

--- components/Layout/Recommendations.vue
+++ components/Layout/Recommendations.vue
@@ -8,7 +8,7 @@
             What colleagues and collaborators have said about working with me.
         </p>
 
-        <div class="flex flex-wrap justify-center gap-10 mt-10">
+        <div v-if="recommendations.length > 0" class="flex flex-wrap justify-center gap-10 mt-10">
             <UiRecommendationCard
                 :recommendations="recommendations"
                 :linkedin-recommendations-url="RECOMMENDATION_URL"
